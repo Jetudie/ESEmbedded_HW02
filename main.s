dc7265538df63f02c443499f33a061115ee7b58c
Jetudie <fxm40007@hotmail.com>
2019-03-07 15:59:11 +0800

    Modify main.s to test push pop

--- main.s
+++ main.s
@@ -6,20 +6,34 @@
 .global _start
 .type _start, %function
 _start:
-	nop
+	//
+	// initialize data in r0-r7
+	//
+	movs	r0, 	#10
+	movs	r1, 	#11
+	movs	r2, 	#12
+	movs	r3, 	#13
+	movs	r4, 	#14
+	movs	r5, 	#15
+	movs	r6, 	#16
+	movs	r7, 	#17
 
 	//
-	//branch w/o link
+	// push and pop
 	//
-	b	label01
+	push	{r0, r1, r2, r3}
 
-label01:
-	nop
+	movs	r0, 	#20
+	movs	r1, 	#21
+	movs	r2, 	#22
+	movs	r3, 	#23
+	
+	push	{r3, r1, r0, r2}
+	
+	pop		{r4, r5, r6, r7}
+	pop		{r6, r5, r7, r4}
 
-	//
-	//branch w/ link
-	//
-	bl	sleep
+	b		sleep
 
 sleep:
 	nop
